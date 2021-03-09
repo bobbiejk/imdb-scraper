5576adee6e72f01c4c8dfcb6c7215ff133b8c1a3
bobbiejk <bobbiejkrijger@gmail.com>
2021-03-09 11:31:40 +0100

    created src for data-preparation

---
+++ src/data-preparation/transform_producers.R
@@ -0,0 +1,31 @@
+originals <- function(){
+
+  # import dataset
+  producers <- read.csv("./gen/data-preparation/input/producers.csv", sep=";")
+  
+  # create data frame colnames
+  streaming_services <- c("Netflix", "Disney+", "Medialand")
+  producers[,streaming_services] <- NA
+  
+  # check whether streaming service is in list of producers
+  for (row in 1:nrow(producers)){
+    for (streaming_service in streaming_services){
+      if (grepl(streaming_service, producers[row,2]) == 1){ 
+        producers[row,streaming_service] <- 1 
+      }
+      else{
+        producers[row, streaming_service] <- 0
+      }
+    }
+  }
+  return(producers)
+}
+
+# get originals
+originals_df  <- originals()
+
+# create directory
+dir.create("./gens/data-preparation/temp")
+
+# save transformed data
+save(originals_df, file= "./gen/data-preparation/temp/transform_producers.RData")
---
+++ src/data-preparation/update_input.R
@@ -0,0 +1,10 @@
+# make directory
+dir.create("gen")
+dir.create("gen/data-preparation")
+dir.create("gen/data-preparation/input")
+
+# copy the raw data into input folder
+file.copy("./data/imdb/content.csv", "./gen/data-preparation/input/content.csv")
+file.copy("./data/imdb/distributors.csv", "./gen/data-preparation/input/distributors.csv")
+file.copy("./data/imdb/producers.csv", "./gen/data-preparation/input/producers.csv")
+file.copy("./data/trakt/watched.csv", "./gen/data-preparation/input/watched.csv")
