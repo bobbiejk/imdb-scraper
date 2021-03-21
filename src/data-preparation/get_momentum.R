71e5ad4592502a4caf06ab0ac72abb16f4cbc5a0
bobbiejk <bobbiejkrijger@gmail.com>
2021-03-21 16:50:47 +0100

    fixed spelling error

--- src/data-preparation/get_momentum.R
+++ src/data-preparation/get_momentum.R
@@ -6,10 +6,10 @@ momentum <- function(){
   #' Momentum is identified when release date and the review data
   #' are at least 7 days in between. 
   
-  # add datasets to transfrm
-  popularity_df <- load("./gen/data-preparation/temp/transform_reviews.RData")
-  simultaneous_df <- load("./gen/data-preparation/temp/transform_releases.RData")
-  
+  # add datasets to transform
+  load("./gen/data-preparation/temp/transform_reviews.RData")
+  load("./gen/data-preparation/temp/transform_releases.RData")
+
   # add a column in order to account for momentum
   popularity_df$momentum <- 0
   
@@ -26,7 +26,7 @@ momentum <- function(){
       # when the ids are equal, then check for that release date of that episode
       # in order to find whether the review date has been written within a week
       if (popularity_df$id[i] == simultaneous_df$imdb_id[j]){
-        if(popularity_df$review_data[i] >= simultaneous_df$release_date[j] & popularity_df$review_data[i] < simultaneous_df$release_date[j] + 7){
+        if(popularity_df$review_data[i] >= simultaneous_df$release_data[j] & popularity_df$review_data[i] < simultaneous_df$release_data[j] + 7){
           popularity_df$momentum[i] <- 1
         }
       }
