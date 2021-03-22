8bc714527c10b54509d2e8b8d62432be34f1b944
bobbiejk <bobbiejkrijger@gmail.com>
2021-03-22 09:12:12 +0100

    left join content to simultaneous df

--- src/data-preparation/update_output.R
+++ src/data-preparation/update_output.R
@@ -1,7 +1,14 @@
+# needed package
+require(dplyr)
+
 # make directory
 dir.create("gen")
 dir.create("gen/data-preparation")
 dir.create("gen/data-preparation/output")
 
+# file load
+load("./gen/data-preparation/temp/transform_releases.RData")
+load("./gen/data-preparation/output/content.RData")
+
 # copy the raw data into input folder
-file.copy("./gen/data-preparation/temp/transform_releases.RData", "./gen/data-preparation/output/simultaneous.RData")
+left_join(simultaneous_df, content_df, by=c("imdb_id"="id"))
