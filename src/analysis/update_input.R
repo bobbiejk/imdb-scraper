62c320d24ad870037461b09ca6036b8a0e9017ee
bobbiejk <bobbiejkrijger@gmail.com>
2021-03-25 14:13:42 +0100

    set input to analysis

--- src/analysis/update_input.R
+++ src/analysis/update_input.R
@@ -4,6 +4,4 @@ dir.create("gen/analysis")
 dir.create("gen/analysis/input")
 
 # copy the raw data into input folder
-file.copy("./gen/data-preparation/output/content.RData", "./gen/analysis/input/content.Rdata")
-file.copy("./gen/data-preparation/output/momentum.RData", "./gen/analysis/input/momentum.Rdata")
-file.copy("./gen/data-preparation/output/simultaneous.RData", "./gen/analysis/input/simultaneous.Rdata")
+file.copy("./gen/data-preparation/output/reviews.RData", "./gen/analysis/input/reviews.Rdata")
